5cc4a94a6c18bd5573ceba0e43f48307355170c7
Lena Hou <lenahou2013@gmail.com>
2021-01-30 21:05:34 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+  while i < array.length
+    yeild(array[i])
+    i += 1
+  end
+    array
+  else
+    puts "No block was given. Please check!"
+  end
+end
+
+my_each(collection) do |i|
+  puts i
 end
